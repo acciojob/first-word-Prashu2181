7b711851cf8f91d00f5f9a146e983d30b95883ea
Prashu2181 <prasannamadireddy466@gmail.com>
2025-02-06 11:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	let spaceIndex = s.IndexOf(" ");
+	if(spaceIndex === -1);{
+	return s;
+	}
+	return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
